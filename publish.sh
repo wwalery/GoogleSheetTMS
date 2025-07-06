91828596d57cbc3f2140dbdb0882f5d796119fc6
Valery Vysotsky <dev@wysotsky.info>
2025-07-06 20:50:31 +0300

    Add dotenv dependency

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 python3 setup.py check
 python3 setup.py sdist
-gpg --detach-sign -a dist/google_sheet_tms-0.1.0.tar.gz 
+gpg --detach-sign -a dist/google_sheet_tms-0.1.2.tar.gz 
 twine upload dist/*
--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'google_sheet_tms',
-    version = '0.1.1',
+    version = '0.1.2',
     author = 'Walery Wysotsky',
     author_email = 'dev@wysotsky.info',
     description = 'Use Google Sheet as TMS',
     packages = ['google_sheet_tms'],
     install_requires = [
+          'dotenv',
           'gspread',
           'oauth2client'
     ],
